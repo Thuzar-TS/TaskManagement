05a1178665f35d39f6e13e325e98a08df4866a9d
ThuzarTS <thuzar.ts92@gmail.com>
2019-07-30 16:33:40 +0630

    Database and Model Created

---
+++ database/migrations/2019_07_30_093140_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('description');
+            $table->tinyInteger('status');
+            $table->integer('project_id');
+            $table->integer('user_id');
+            $table->string('start_date');
+            $table->string('due_date');
+            $table->string('end_date');            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
