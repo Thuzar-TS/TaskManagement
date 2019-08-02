31d4ad683accfaa5023b18ad17c7778bcd167dc6
ThuzarTS <thuzar.ts92@gmail.com>
2019-08-02 16:33:36 +0630

    Db seed Faker created

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,54 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Models\Task;
+use App\Models\Project;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
+        $faker = Faker::create();
+        for ($i=0; $i < 10; $i++) { 
+            $task = new Task();
+            $task->title = $faker->sentence();
+            $task->description = $faker->paragraph();
+            $task->project_id = rand(1, 10);
+            $task->user_id = rand(1, 2);
+            $task->status = 0;
+            $task->start_date = '2019-08-02';
+            $task->due_date = '2019-08-02';
+            $task->end_date = '2019-08-02';
+            $task->save();
+        }
+
+        for ($i=0; $i < 10; $i++) { 
+            $project = new Project();
+            $project->name = $faker->sentence();
+            $project->description = $faker->paragraph();
+            $project->user_id = rand(1, 2);
+            $project->save();
+        }
+
+        $user = new User();
+        $user->name = "Bob";
+        $user->email = "bob@gmail.com";
+        $user->password = bcrypt("123123");
+        $user->save();
+
+        $user = new User();
+        $user->name = "Alice";
+        $user->email = "alice@gmail.com";
+        $user->password = bcrypt("123123");
+        $user->save();
+        
         // $this->call(UsersTableSeeder::class);
     }
 }
